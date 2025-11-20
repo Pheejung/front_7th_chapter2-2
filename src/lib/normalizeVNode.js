e9b385d08d3aaa0caa0e928aba84e25217d33daa
Pheejung <gmlwjd@naver.com>
2025-11-21 01:32:37 +0900

    feat: normalizeVNode 작성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,37 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode == null || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode === "string") {
+    return vNode;
+  }
+
+  if (Array.isArray(vNode)) {
+    return vNode
+      .map(normalizeVNode)
+      .filter((child) => child !== "" && child != null);
+  }
+
+  if (typeof vNode.type === "function") {
+    const props = {
+      ...vNode.props,
+      children: vNode.children.length > 0 ? vNode.children : undefined,
+    };
+
+    const result = vNode.type(props);
+
+    return normalizeVNode(result);
+  }
+
+  return {
+    ...vNode,
+    children: vNode.children
+      .map(normalizeVNode)
+      .filter((child) => child !== "" && child != null),
+  };
 }
